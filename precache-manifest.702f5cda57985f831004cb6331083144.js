46ca90cc263ac4c12887f9a03f6306be7decb986
MinLee <minlee0702@gmail.com>
2020-04-30 16:21:26 -0500

    Updates

---
+++ precache-manifest.702f5cda57985f831004cb6331083144.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd510c63d4abbadc00dc2777edb93aa2",
+    "url": "/github-hosting/index.html"
+  },
+  {
+    "revision": "c0d8c108aa372828cb7d",
+    "url": "/github-hosting/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0532177cebc74655499c",
+    "url": "/github-hosting/static/js/2.f35d01e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-hosting/static/js/2.f35d01e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0d8c108aa372828cb7d",
+    "url": "/github-hosting/static/js/main.70c3b8dc.chunk.js"
+  },
+  {
+    "revision": "778b3ef75a068e8b579d",
+    "url": "/github-hosting/static/js/runtime-main.68219f8d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-hosting/static/media/logo.5d5d9eef.svg"
+  }
+]);
